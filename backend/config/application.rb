38ae7fdbf6b59902eca1a8bec5e099c4da587caa
神代泰宏 <yasuhiro79367s@hotmail.co.jp>
2023-05-03 01:03:29 +0900

    Cross-Originの対応追加

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -2,6 +2,9 @@ require_relative "boot"
 
 require "rails/all"
 
+require 'dotenv'
+Dotenv.load
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -10,6 +13,13 @@ module GoalManager
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins ENV["API_DOMAIN"]
+        resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      end
+    end
 
     # Configuration for the application, engines, and railties goes here.
     #
