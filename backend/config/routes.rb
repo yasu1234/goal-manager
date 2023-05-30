4ff1b9cdb0a96181a0d0b5905b1410e4a255c5f4
神代泰宏 <yasuhiro79367s@hotmail.co.jp>
2023-05-31 01:26:18 +0900

    会員登録用のルーティング追加

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  mount_devise_token_auth_for 'User', at: 'auth', controllers: {
+    registrations: 'signup'
+  }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
