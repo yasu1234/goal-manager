a22c8ec44e84e79c2282ff0af8a4eebf5e792dc6
神代泰宏 <yasuhiro79367s@hotmail.co.jp>
2023-05-31 00:32:38 +0900

    ヘッダーの値を取得できるようにexposeに設定の追加

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -17,7 +17,10 @@ module GoalManager
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins ENV["API_DOMAIN"]
-        resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
+        resource '*',
+        headers: :any,
+        expose: ["access-token", "expiry", "token-type", "uid", "client"],
+        methods: [:get, :post, :put, :patch, :delete, :options, :head]
       end
     end
 
