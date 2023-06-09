a69942d664df4b0dd6f6f387c41dbd4dea5b69c9
神代泰宏 <yasuhiro79367s@hotmail.co.jp>
2023-06-09 22:07:21 +0900

    ログイン画面の作成

---
+++ client/src/components/Login.vue
@@ -0,0 +1,112 @@
+<script>
+import axios from 'axios'
+export default {
+    data () {
+        return {
+            email: '',
+            password: '',
+            isPasswordValidationShow: false,
+
+            validation: {
+                passwordMatchResult: ''
+            }
+        }
+    },
+    methods: {
+        async login() {
+            try {
+                const res = await axios.post(import.meta.env.VITE_APP_API_BASE + '/auth/sign_in', {
+                    email: this.email,
+                    password: this.password,
+                })
+                console.log({ res })
+                return res
+            } catch (error) {
+                console.log({ error })
+            }
+        },
+    
+        async checkValidate() {
+            await this.login()
+        }
+    }
+}
+</script>
+
+<template>
+    <h1 class="signUpTitle">ログイン</h1>
+    <div class="singUpInput">
+        <form class="form" @submit.prevent="checkValidate">
+            <div class="item">
+                <label class="itemLabel">メールアドレス</label>
+                <input id="email" type="email" v-model="email">
+            </div>
+            <div class="item">
+                <label class="itemLabel" for="password">パスワード</label>
+                <input id="password" type="password" v-model="password">
+            </div>
+            <div class="error-message" v-show="isPasswordValidationShow">
+                <p class="error-message-text">{{ validation.passwordMatchResult }}</p>
+            </div>
+            <div class="signUpTitle">
+                <button class="loginButton">ログイン</button>
+            </div>
+        </form>
+    </div>
+</template>
+
+<style>
+.form{
+   width: 100%;
+   margin:0 auto;
+   padding: 50px;
+   box-sizing: border-box;
+}
+
+.signUpTitle {
+    padding-top: 40px;
+    text-align: center;
+}
+ 
+.item{
+    padding-top: 40px;
+    width: 50%;
+    margin: 0 auto;
+}
+
+.itemLabel{
+    display: block;
+    text-align: left;
+}
+
+.error-message{
+    width: 50%;
+    margin: 0 auto;
+}
+ 
+.error-message-text{
+    color: red;
+    font-size: 15px;
+}
+
+.form input[type="email"],
+.form input[type="password"] {
+     padding: 10px;
+     width: 100%;
+ }
+ 
+.form textarea{
+   padding: 10px;
+   height: 100px;
+   font-size: 14px;
+   width: 50%;
+ }
+ 
+.loginButton{
+    background: #ffa500;
+    color: white;
+    font-size:16px;
+    font-weight:bold;
+    padding: 10px 50px;
+}
+</style>
