8b15b17173616e82103ed0ab1a2a34bc85b0f502
神代泰宏 <yasuhiro79367s@hotmail.co.jp>
2023-07-31 09:45:53 +0900

    vuetifyをプロジェクトで使えるように設定

--- client/src/main.js
+++ client/src/main.js
@@ -3,4 +3,26 @@ import './style.css';
 import App from './App.vue';
 import router from './router';
 
-createApp(App).use(router).mount('#app')
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import { aliases, mdi } from 'vuetify/iconsets/mdi-svg';
+
+const app = createApp(App)
+
+const vuetify = createVuetify({
+    components,
+    directives,
+    icons: {
+        defaultSet: 'mdi',
+        aliases,
+        sets: {
+          mdi,
+        },
+      },
+})
+
+app.use(vuetify)
+app.use(router)
+app.mount('#app')
