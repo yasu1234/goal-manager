dbb38dda19d6594731af30cd3dfb52eb6d343c44
神代泰宏 <yasuhiro79367s@hotmail.co.jp>
2023-08-02 09:53:30 +0900

    サイドバーの追加

---
+++ client/src/components/SideBar.vue
@@ -0,0 +1,61 @@
+<script>
+import axios from 'axios';
+import Cookies from 'js-cookie'
+import { mdiChevronLeft } from '@mdi/js'
+import { mdiViewList } from '@mdi/js'
+
+export default {
+    data() {
+        return {
+            items: [
+                { index: 0, title: '一覧', icon: mdiViewList, path: '/login' }
+            ],
+            drawer: true,
+            rail: false,
+            mdiChevronLeft,
+            mdiViewList
+        }
+    },
+    mounted() {
+    },
+    computed: {},
+    methods: {
+    }, 
+};
+</script>
+
+<template>
+    <v-navigation-drawer
+        v-model="drawer"
+        :rail="rail"
+        permanent
+        @click="rail = false"
+      >
+        <v-list-item
+          prepend-avatar="src/assets/image/home_image.jpg"
+          title="Goal Manager"
+          nav
+        >
+          <template v-slot:append>
+            <v-btn
+              variant="text"
+              :icon= "mdiChevronLeft"
+              @click.stop="rail = !rail"
+            ></v-btn>
+          </template>
+        </v-list-item>
+
+        <v-divider></v-divider>
+
+        <v-list density="compact" nav>
+          <v-list-item v-for="item in items"
+          :key="item.index"
+          :to="item.path"
+          :title="item.title"
+          :prepend-icon= "item.icon" value="home" />
+        </v-list>
+      </v-navigation-drawer>
+</template>
+
+<style>
+</style>
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 import Home from '/src/components/Home.vue';
 import SignUp from '/src/components/SignUp.vue';
 import Login from '/src/components/Login.vue';
+import GoalManager from '/src/components/GoalManager.vue';
 
 const routes = [
   {
@@ -18,6 +19,14 @@ const routes = [
     path: '/login', 
     name: 'Login', 
     component: Login,
+  },
+  {
+    path: '/goalManager', 
+    name: 'GoalManager', 
+    component: GoalManager,
+    children: [
+      { path: '/login', name:'Login', component: Login }
+    ]
   },
 ];
 
