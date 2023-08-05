6456f6cf1f947e1d66fba76309052144698ddb97
神代泰宏 <yasuhiro79367s@hotmail.co.jp>
2023-08-05 22:29:57 +0900

    mavon-editorのライブラリ追加

--- client/src/main.js
+++ client/src/main.js
@@ -2,12 +2,13 @@ import { createApp } from 'vue';
 import './style.css';
 import App from './App.vue';
 import router from './router';
-
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 import { aliases, mdi } from 'vuetify/iconsets/mdi-svg';
+import mavonEditor from 'mavon-editor'
+import 'mavon-editor/dist/css/index.css' 
 
 const app = createApp(App)
 
@@ -25,4 +26,5 @@ const vuetify = createVuetify({
 
 app.use(vuetify)
 app.use(router)
+app.use(mavonEditor)
 app.mount('#app')
