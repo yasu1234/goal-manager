0a27c806f42b106eedb722ae97cc321338226722
神代泰宏 <yasuhiro79367s@hotmail.co.jp>
2023-08-19 19:01:21 +0900

    カテゴリーテーブルの作成

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_29_133237) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_19_085954) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", primary_key: "category_id", id: :serial, force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "provider", default: "email", null: false
