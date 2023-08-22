4b074b5acb535d7ebe7a6aac928d2fc4a76d148b
神代泰宏 <yasuhiro79367s@hotmail.co.jp>
2023-08-23 03:50:32 +0900

    目標登録用のController作成

---
+++ backend/app/controllers/goals_controller.rb
@@ -0,0 +1,12 @@
+class GoalsController < ApplicationController
+    before_action :authenticate_user!
+    def create
+        @user = current_user
+        @goal = Goal.create(goal_params)
+        render json: { goal: @goal }, status: 200
+    end
+
+    def goal_params
+        params.permit(:title, :description, :category_id, :start_date, :end_date, :create_user_id)
+    end    
+end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
 
   resources :category, only: [:index, :show]
 
+  resources :goals
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
