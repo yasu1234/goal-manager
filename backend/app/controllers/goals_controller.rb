6485c8cc4bcd78b0da565bd898c84203e2e70008
神代泰宏 <yasuhiro79367s@hotmail.co.jp>
2023-09-13 22:30:37 +0900

    目標登録の処理追加

--- backend/app/controllers/goals_controller.rb
+++ backend/app/controllers/goals_controller.rb
@@ -1,5 +1,5 @@
 class GoalsController < ApplicationController
-    before_action :authenticate_user!
+    # before_action :authenticate_user!
     def create
         @user = current_user
         @goal = Goal.create(goal_params)
@@ -7,6 +7,6 @@ class GoalsController < ApplicationController
     end
 
     def goal_params
-        params.permit(:title, :description, :category_id, :start_date, :end_date, :create_user_id)
+        params.permit(:title, :description, :category_id, :start_date, :end_date, :create_user_id, :images)
     end    
 end
--- backend/app/models/goal.rb
+++ backend/app/models/goal.rb
@@ -1,2 +1,3 @@
 class Goal < ApplicationRecord
+    has_many_attached :images
 end
