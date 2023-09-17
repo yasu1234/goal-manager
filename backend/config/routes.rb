151a2359ae7e62968cd85c487be845a0ed1631ac
神代泰宏 <yasuhiro79367s@hotmail.co.jp>
2023-09-17 13:32:17 +0900

    自分の目標一覧取得メソッドのルーティング作成

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -13,6 +13,8 @@ Rails.application.routes.draw do
 
   resources :goals
 
+  get '/myGoals', to: 'goals#get', defaults: { format: :json }
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
